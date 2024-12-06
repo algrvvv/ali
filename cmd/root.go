d43924a6805712a9e54b739c7d587244c2040d78
p4xt3r <121817930+algrvvv@users.noreply.github.com>
2024-12-06 12:33:14 +0300

    добавлен рут

---
+++ cmd/root.go
@@ -0,0 +1,141 @@
+/*
+Copyright © 2024 algrvvv <alexandrgr25@gmail.com>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+package cmd
+
+import (
+	"fmt"
+	"maps"
+	"os"
+	"path/filepath"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+
+	"github.com/algrvvv/ali/logger"
+	"github.com/algrvvv/ali/utils"
+)
+
+const localConfig = ".ali"
+
+var (
+	debug bool
+
+	// rootCmd represents the base command when called without any subcommands
+	rootCmd = &cobra.Command{
+		Use:               "ali",
+		Short:             "A brief description of your application",
+		Args:              cobra.ArbitraryArgs,
+		ValidArgsFunction: getAliases,
+		Run: func(_ *cobra.Command, args []string) {
+			if len(args) == 0 {
+				fmt.Println("use help for see usage")
+				return
+			}
+			alias := args[0]
+			params := args[1:]
+
+			logger.SaveDebugf("got command: %s", alias)
+
+			command := utils.GetAlias(alias)
+			logger.SaveDebugf("got command: %s", command)
+
+			utils.ExecuteAlias(command, params)
+		},
+	}
+)
+
+func getAliases(
+	cmd *cobra.Command,
+	args []string,
+	toComplete string,
+) ([]string, cobra.ShellCompDirective) {
+	aliases, ok := viper.Get("aliases").(map[string]any)
+	if !ok {
+		fmt.Println("failed to get all aliases")
+		return []string{}, cobra.ShellCompDirectiveNoFileComp
+	}
+
+	var res []string
+	for k := range maps.Keys(aliases) {
+		res = append(res, k)
+	}
+
+	return res, cobra.ShellCompDirectiveNoFileComp
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	cobra.OnInitialize(initConfig, initLogger, initLocalConfig)
+
+	// Here you will define your flags and configuration settings.
+	// Cobra supports persistent flags, which, if defined here,
+	// will be global for your application.
+
+	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.ali.yaml)")
+
+	// Cobra also supports local flags, which will only run
+	// when this action is called directly.
+	rootCmd.PersistentFlags().BoolVarP(&debug, "debug", "D", false, "print debug messages")
+}
+
+func initConfig() {
+	home, err := os.UserHomeDir()
+	utils.CheckError(err)
+
+	path := filepath.Join(home, ".ali")
+	viper.AddConfigPath(path)
+	viper.SetConfigType("toml")
+	viper.SetConfigName("config")
+
+	viper.AutomaticEnv()
+	if err := viper.ReadInConfig(); err != nil {
+		utils.CheckError(err)
+	}
+	logger.SaveDebugf("using config: %s", viper.ConfigFileUsed())
+}
+
+func initLogger() {
+	home, err := os.UserHomeDir()
+	utils.CheckError(err)
+
+	path := filepath.Join(home, ".ali/ali.log")
+	err = logger.NewLogger(path, &logger.Options{Debug: true, MoreInfo: false, Stdout: debug})
+	utils.CheckError(err)
+}
+
+func initLocalConfig() {
+	viper.SetConfigName(".ali")
+	viper.AddConfigPath(".")
+
+	if err := viper.MergeInConfig(); err != nil {
+		logger.SaveDebugf("local config not found")
+	}
+	logger.SaveDebugf("local config loaded")
+}
