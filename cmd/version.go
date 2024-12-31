da19c115702f439d74eb7f632c10eca067d71aa6
p4xt3r <121817930+algrvvv@users.noreply.github.com>
2024-12-31 15:22:38 +0300

    обновлена версия

--- cmd/version.go
+++ cmd/version.go
@@ -29,7 +29,7 @@ import (
 
 const (
 	logo    = "       _ _ \n  __ _| (_)\n / _` | | |\n| (_| | | |\n \\__,_|_|_|"
-	version = "v1.0"
+	version = "v1.3.5"
 )
 
 // versionCmd represents the version command
