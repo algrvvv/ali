c8f295b1f1300d1b0a5a538e428560d717a02a0d
p4xt3r <121817930+algrvvv@users.noreply.github.com>
2025-03-29 14:41:04 +0300

    добавлена тулза для автоопределения формата конфигурационного файла

---
+++ utils/get_configuration_type.go
@@ -0,0 +1,47 @@
+package utils
+
+import (
+	"encoding/json"
+	"errors"
+	"os"
+
+	"github.com/algrvvv/ali/logger"
+
+	"github.com/pelletier/go-toml/v2"
+	"gopkg.in/yaml.v3"
+)
+
+const (
+	YamlConfigurationType = "yaml"
+	JsonConfigurationType = "json"
+	TomlConfigurationType = "toml"
+)
+
+var ErrUnsupportedConfigType = errors.New("unsupported config type")
+
+// GetConfigurationType функция для определения типа конфигурации
+func GetConfigurationType(path string) (string, error) {
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return "", nil
+	}
+
+	if json.Valid(data) {
+		return JsonConfigurationType, nil
+	}
+	logger.SaveDebugf("failed to read config [%s] as json", path)
+
+	var yamlData map[string]any
+	if err = yaml.Unmarshal(data, &yamlData); err == nil {
+		return YamlConfigurationType, nil
+	}
+	logger.SaveDebugf("failed to read config [%s] as yaml: %v", path, err)
+
+	var tomlData map[string]any
+	if err = toml.Unmarshal(data, &tomlData); err == nil {
+		return TomlConfigurationType, nil
+	}
+	logger.SaveDebugf("failed to read config [%s] as toml: %v", path, err)
+
+	return "", ErrUnsupportedConfigType
+}
