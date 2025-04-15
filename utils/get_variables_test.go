06abee941dac83e128bdad4aaadabf507fee6f34
p4xt3r <121817930+algrvvv@users.noreply.github.com>
2025-04-15 11:57:49 +0300

    исправлен тест на проверку применения переменных

--- utils/get_variables_test.go
+++ utils/get_variables_test.go
@@ -8,28 +8,29 @@ import (
 
 func TestGetVariables(t *testing.T) {
 	vars := map[string]string{
-		"EXECUTE":   "./twentyone",
-		"USER_FLAG": "--user=algrvvv",
-		"NAME":      "go run main.go",
-		"COMMAND":   "list -L",
+		"execute":   "./twentyone",
+		"user_flag": "--user=algrvvv",
+		"name":      "go run main.go",
+		"command":   "list -L",
 	}
 
 	tests := []struct {
 		input    string
 		expected string
 	}{
-		{input: "{{EXECUTE}} {{USER_FLAG}}", expected: "./twentyone --user=algrvvv"},
-		{input: "{{EXECUTE}} run", expected: "./twentyone run"},
-		{input: "go run main.go {{COMMAND}}", expected: "go run main.go list -L"},
-		{input: "{{NAME}} {{COMMAND}} -f", expected: "go run main.go list -L -f"},
-		{input: "{{MISSING}} run", expected: "{{MISSING}} run"},
+		{input: "{{execute}} {{user_flag}}", expected: "./twentyone --user=algrvvv"},
+		{input: "{{execute}} run", expected: "./twentyone run"},
+		{input: "go run main.go {{command}}", expected: "go run main.go list -L"},
+		{input: "{{name}} {{command}} -f", expected: "go run main.go list -L -f"},
+		{input: "{{missing}} run", expected: "{{missing}} run"},
 	}
 
 	for _, test := range tests {
 		got := utils.GetVariables(test.input, vars)
 		if got != test.expected {
-			t.Errorf("want: %s; got: %s", test.expected, got)
+			t.Errorf("ERROR: want: %s; got: %s", test.expected, got)
+		} else {
+			t.Logf("SUCCESS! got: %s", got)
 		}
-		t.Logf("SUCCESS! got: %s", got)
 	}
 }
