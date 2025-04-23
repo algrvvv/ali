7009b249cbd86b908317b8cb9eb3e7c764f9dec5
p4xt3r <121817930+algrvvv@users.noreply.github.com>
2025-04-23 18:17:31 +0300

    реализован запуск и выполнение одиночных и параллельных команд

---
+++ local/execute_local.go
@@ -0,0 +1,35 @@
+package local
+
+import (
+	"fmt"
+
+	"github.com/algrvvv/ali/utils"
+)
+
+func ExecuteLocal(
+	command string, params []string,
+	flags map[string]string, envs map[string]any,
+	printResultCommands bool,
+) error {
+	cmd, err := utils.PrepareCommand(
+		command,
+		params,
+		flags,
+		envs,
+		printResultCommands,
+	)
+	if err != nil {
+		return fmt.Errorf("failed to create command instance: %w", err)
+	}
+
+	err = cmd.Start()
+	if err != nil {
+		return fmt.Errorf("failed to start exec command: %w", err)
+	}
+
+	if err = cmd.Wait(); err != nil {
+		return fmt.Errorf("failed to wait command: %w", err)
+	}
+
+	return nil
+}
---
+++ parallel/execute_parallel.go
@@ -0,0 +1,69 @@
+package parallel
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"strings"
+	"sync"
+	"syscall"
+
+	"github.com/algrvvv/ali/utils"
+)
+
+func ExecuteParallel(
+	entry *utils.AliasEntry, params []string,
+	flags map[string]string, envs map[string]any,
+	printResultCommands bool,
+) {
+	wg := &sync.WaitGroup{}
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt, syscall.SIGTERM)
+
+	if printResultCommands {
+		fmt.Println("Configured commands:")
+		for _, cmd := range entry.Cmds {
+			fmt.Printf("[%s] -> %s\n", fmt.Sprintf("%s%s%s", utils.Colors["blue"], entry.AliasName, utils.Colors["reset"]), cmd)
+		}
+		fmt.Println(strings.Repeat("=", 30))
+		fmt.Println()
+	}
+
+	for _, command := range entry.Cmds {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+
+			cmd, err := utils.PrepareCommand(
+				command,
+				params,
+				flags,
+				envs,
+				printResultCommands,
+			)
+			if err != nil {
+				fmt.Printf("failed to prepare command: [%s]\n", command)
+				return
+			}
+
+			if err := cmd.Start(); err != nil {
+				fmt.Printf("failed to start command: [%s]\n", command)
+				return
+			}
+
+			if err := cmd.Wait(); err != nil {
+				fmt.Printf("failed to start command: [%s]\n", command)
+				return
+			}
+		}()
+	}
+
+	go func() {
+		<-signalChan
+		fmt.Println("got interrupt...")
+		wg.Wait()
+		os.Exit(1)
+	}()
+
+	wg.Wait()
+}
